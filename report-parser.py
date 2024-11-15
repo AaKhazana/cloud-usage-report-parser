35a7ed88852c72de6afd787ac5e876140f68c2a5
AaKhazana <aa.khazana@gmail.com>
2024-11-15 15:09:20 +0500

    updated host to serve on all networks

--- report-parser.py
+++ report-parser.py
@@ -126,4 +126,8 @@ def upload_file():
         return parsed_report_data
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+        )
