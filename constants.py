9c85e566adc97863c2e3cf7d844e79ac7ae2195a
MhAhmadAli <fa17-bsse-023@lgu.edu.pk>
2024-12-10 12:46:19 +0500

    UPDATE: updated ecs to categorise shared and clustered instances

---
+++ constants.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+MONTH_31_DAYS = [1, 3, 5, 7, 8, 10, 12]
+MONTH_30_DAYS = [4, 6, 9, 11]
+MONTH_28_DAYS = [2]
+
+STANDARD_CLOUD_HOURS = 730
+
+HOURS_31_DAYS = 31 * 24
+HOURS_30_DAYS = 30 * 24
+HOURS_28_DAYS = 28 * 24
+
+class ResourceType(Enum):
+    ECS = "ecs"
+    EVS = "evs"
+    EIP = "eip"
+    EIP_BANDWIDTH = "eip-bandwidth"
+    ELB = "elb"
+    NAT = "nat-gateway"
+    VPN = "virtual-private-network"
+    VPN_BANDWIDTH = "vpn-bandwidth"
+
+
+class StorageType(Enum):
+    SSD = "ssd"
+    HDD = "hdd"
+
+
+class ServiceTag(Enum):
+    CLUSTERED = "clustered"
+    DEDICATED = "dedicated"
